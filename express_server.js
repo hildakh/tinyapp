1d18eb6f6d596815fb1e3018a625838d322c18a2
hildakh <h_khajehamiri@yahoo.com>
2019-10-29 02:35:20 +0000

    installed body-parser and added it to express_server

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;  //default port apparently
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs'); //setting ejs as the view engine after installing ejs
 
