3c146058b7ec116b422c336ef1f63ff2385c43b6
hildakh <h_khajehamiri@yahoo.com>
2019-11-03 17:12:49 +0000

    for some reason the second test fails. Added console.log to compare the output and they are the samegit add helpersTest.js  Will look into it later

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -3,29 +3,31 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
-    return user == expectedOutput;// Write your assert statement here
+    console.log(user, expectedOutput);
+    assert(user, expectedOutput);// Write your assert statement here
   });
 });
 
-describe('getUserByEmail', function () {
-  it('should return undefined if the email is not found in the usersDatabase', function () {
-    const user = getUserByEmail("hildakh@lhl.com", testUsers)
+describe('getUserByEmail', function() {
+  it('should return undefined if the email is not found in the usersDatabase', function() {
+    const user = getUserByEmail('hildakhl@lhl.com', testUsers);
     const expectedOutput = undefined;
-    return user == expectedOutput;// Write your assert statement here
+    console.log(user, expectedOutput);
+    assert(user, expectedOutput);// Write your assert statement here
   });
 });
